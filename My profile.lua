f167f11f1d42618ec9dbfeeabe80a4be9208d099
𝐒𝐔𝐆𝐀𝐑 <roggps9@gmail.com>
2025-03-17 23:24:52 +0700

    Create My profile.lua

---
+++ My profile.lua
@@ -0,0 +1,11 @@
+<div align="center">
+<img src="https://typograssy.deno.dev/api?text=Chompu%20Discord%20App%20%20&l0=none&l1=ecb6e5&l2=ffadef&l3=ffd6f4&l4=ff94db&bg=none&frame=none&speed=100&comment=">
+</div>
+
+### <p align="center">Developer for Project<p>
+
+<div align="center">
+
+<a href="https://discord.com/users/919878532228841532"><img align="center"  width="300px" src="https://lanyard.cnrad.dev/api/919878532228841532"></a>
+
+</div>
